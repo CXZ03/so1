6f30013ba534af5a1b81786b7fc651d651b31a6e
cxz03 <chengxz0393@gmail.com>
2023-10-05 18:30:25 +0200

    Semana 1, hecho las funciones, falta testear

---
+++ my_lib.c
@@ -0,0 +1,57 @@
+#include "my_lib.h"
+
+size_t my_strlen(const char *str){
+    const char *aux_str = str;
+    int i = 0;
+    while(str[i++]){
+    }
+    return aux_str - str;
+}
+
+int my_strcmp(const char *str1, const char *str2){
+    int i=0;
+    while(str1[i++] == str2[i++]){
+        if(str1[i] == "\0" && str2[i] == "\0")
+        return 0;
+    }
+    return str1[i] - str2[i];
+}
+
+char *my_strcpy(char *dest, const char *src){
+    for ( i = 0; src[i] != "\0"; i++)
+    {
+        dest[i]=src[i];
+    }
+    dest[++i] == "\0"
+    return &dest;
+    
+}
+
+char *my_strncpy(char *dest, const char *src, size_t n){
+    for ( i = 0; i < n; i++)
+    {
+        dest[i]=src[i];
+    }
+    dest[++i] == "\0"
+    return &dest;
+}
+
+char *my_strcat(char *dest, const char *src){
+    int dest_size = my_strlen(*dest);
+    for ( i = 0; src[i] != "\0"; i++)
+    {
+        dest[dest_size + i] = src[i];
+    }
+    dest[++i] == "\0"
+    return &dest;
+}
+
+char *my_strchr(const char *s, int c){
+    const char *aux_s = s;
+    while(*aux_s++ != c){
+        if(*aux_s){
+            return null;
+        }
+    }
+    return (char*)aux_s;
+}
